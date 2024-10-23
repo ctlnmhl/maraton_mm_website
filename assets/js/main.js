ba8a38ce05f87b3ed7bcade55ef432b6de0ff6a9
Catalin <ctlnmhl@gmail.com>
2024-10-23 13:53:07 +0300

    Modificare meniu

--- assets/js/main.js
+++ assets/js/main.js
@@ -398,4 +398,43 @@
 						$main._show(location.hash.substr(1), true);
 					});
 
+		// // Dropdowns.
+		// $('#nav > ul').dropotron({
+		// 	mode: 'fade',
+		// 	noOpenerFade: true,
+		// 	alignment: 'center',
+		// 	detach: false
+		// });
+
+		// Nav.
+
+	// Title Bar.
+		$(
+			'<div id="titleBar">' +
+				'<a href="#navPanel" class="toggle"></a>' +
+				'<span class="title">' + $('#logo h1').html() + '</span>' +
+			'</div>'
+		)
+			.appendTo($body);
+
+	// Panel.
+		$(
+			'<div id="navPanel">' +
+				'<nav>' +
+					$('#nav').navList() +
+				'</nav>' +
+			'</div>'
+		)
+			.appendTo($body)
+			.panel({
+				delay: 500,
+				hideOnClick: true,
+				hideOnSwipe: true,
+				resetScroll: true,
+				resetForms: true,
+				side: 'left',
+				target: $body,
+				visibleClass: 'navPanel-visible'
+			});
+
 })(jQuery);
